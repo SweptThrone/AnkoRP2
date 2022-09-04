674b11256127224d86199f834605501acb6b16c0
SweptThrone <im@sweptthr.one>
2022-09-04 00:04:13 -0500

    less copypasta

--- AnkoRP_MasterAddon/lua/autorun/sh_fixgoldg11.lua
+++ AnkoRP_MasterAddon/lua/autorun/sh_fixgoldg11.lua
@@ -1,4 +1,4 @@
-hook.Add( "TFA_GetStat", "FixGoldG11", function( wep, stat, value)
+hook.Add( "TFA_GetStat", "FixGoldG11", function( wep, stat, value )
     if wep:GetClass() == "tfa_cso_g11g" and stat == "Primary.IronAccuracy" then
         return value / 10
     end
